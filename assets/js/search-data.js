ce6d0e6a9873159d797c0cb2377e737af2f70b90
eddykang06 <eddykang06@gmail.com>
2026-03-31 05:36:11 +0000

    Deploying to gh-pages from @ eddykang06/eddykang06.github.io@323d99b5c233d4178c3e6b52adca8650744cb4b9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,9 +461,9 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "teachings-microbe-mission-science-olympiad",
-          title: 'Microbe Mission (Science Olympiad)',
-          description: "These are lectures I made to prepare high school students for &quot;Microbe Mission&quot;, a Science Olypiad event covering microbiology topics including microbial physiology, genetics, ecology, metabolis, and culture/lab techniques.",
+            },},{id: "teachings-",
+          title: '',
+          description: "",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/data-science-fundamentals/";
             },},{id: "teachings-introduction-to-machine-learning",
